ccba13878a3a3dcdb64cb1212f584daa8b92ee32
Саша Ильина <alexandrai20162008@gmail.com>
2025-12-24 13:03:41 +0300

    added crossword numbering

--- src/classes/Crossword.js
+++ src/classes/Crossword.js
@@ -1,26 +1,22 @@
 export class Crossword{
     constructor(addedWords, skippedWords, grid){
-        let countId = 1;
         this.verticalWords = addedWords.filter((w) => {return w.direction === "vertical"});
         this.verticalWords.sort((a,b) => {
             if(a.coordinates.start_col !== b.coordinates.start_col) return a.coordinates.start_col - b.coordinates.start_col;
             else return a.coordinates.start_row - b.coordinates.start_row;
         })
-        this.verticalWords.forEach(word => {
-            word.id = countId;
-            countId++;
-        });
 
         this.horizontalWords = addedWords.filter((w) => {return w.direction === "horizontal"});
         this.horizontalWords.sort((a,b) => {
             if(a.coordinates.start_row !== b.coordinates.start_row) return a.coordinates.start_row - b.coordinates.start_row
             else return a.coordinates.start_col - b.coordinates.start_col;
         });
-        
-        this.horizontalWords.forEach(word => {
-            word.id = countId;
-            countId++;
-        });
+
+        this.sortedWords = addedWords.sort((a,b) => {
+            if(a.coordinates.start_row !== b.coordinates.start_row) return a.coordinates.start_row - b.coordinates.start_row;
+            else return a.coordinates.start_col - b.coordinates.start_col;
+        })
+        console.log(this.sortedWords);
 
         this.wordAmount = addedWords.length;
         this.grid = grid;
