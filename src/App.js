c7f193bb4345e4bb0be724861f89f57909bedc42
Саша Ильина <alexandrai20162008@gmail.com>
2025-12-27 10:36:59 +0300

    improved wordValidation.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App(){
     setError("");
     
     if(inputValue !== ""){
-      var words = inputValue.trim().split(" ");
+      var words = inputValue;
       words = validateWords(words);
       console.log(words);
 
@@ -35,7 +35,7 @@ function App(){
       const crosswordError = wordsErrorHandler(words, crossword);
       if(crosswordError !== null){
         setError(crosswordError);
-        return
+        return;
       } 
   
       setCrosswordTable(CreateCrosswordTable(crossword));
@@ -54,14 +54,16 @@ function App(){
           <h2>Введите слова:</h2>
           <br></br>
           <textarea autoFocus onChange={(e) => {setInputValue(e.target.value)}} type='text' name='wordInput' className='wordInput'></textarea>
-          <button style={{position: "relative"}} className='infoBtn' onMouseEnter={() => setHint(true)} onMouseLeave={() => setHint(false)}><img src={infoIcon} alt='info'></img></button>
+          <button style={{position: "relative"}} className='infoBtn' onMouseEnter={() => setHint(true)} onMouseLeave={() => setHint(false)}>
+              <img src={infoIcon} alt='info'></img>
+          </button>
           {showHint && <span className='hint'>
             Правила ввода слов:
             <ul style={{margin: '5px 0', paddingLeft: '20px'}}>
               <li>Только буквы русского/английского алфавита</li>
               <li>Цифры и спецсимволы запрещены</li>
               <li>Минимум 2 слова для создания кроссворда</li>
-              <li>Разделяйте слова пробелами или знаками препинания с пробелами</li>  
+              <li>Разделяйте слова пробелами или знаками препинания</li>  
             </ul>
           </span>}
         </div>
--- src/utils/wordValidation.js
+++ src/utils/wordValidation.js
@@ -1,25 +1,53 @@
-export function validateWords(words){
+import { wordsErrorHandler } from "./errorHandler";
+
+const MIN_WORDS_COUNT = 2;
+const MIN_CHARS_COUNT = 2;
+
+export function validateWords(input){
+    input = input.trim();
+    let words = '';
+
+    if(input.split(" ").length < MIN_WORDS_COUNT){
+        let maxWords = 0;
+        let maxSep = ' ';
+        for(let i = 0; i < separators.length; i++){
+            let sep = separators[i];
+            console.log(`separator: ${sep}`);
+            let curLength = input.split(sep).length;
+            if(maxWords < curLength && curLength >= MIN_WORDS_COUNT){
+                maxSep = sep;
+                console.log(maxSep);
+            }
+        }
+        words = input.split(maxSep);
+        console.log(words);
+    }
+    else{
+        words = input.split(" ");
+    }
+    console.log(words);
+
     var validWords = []
     words.forEach(word =>{
         let valid = true;
-        if(word.length >= 2){
-            let trimmedWord = word.trim();
-            let chars = trimmedWord.split("");
+        let trimmedWord = word.trim();
+        let chars = trimmedWord.split("");
 
-            if(chars.length > 0 && separators.includes(chars[chars.length-1])){
-                chars.pop();
-            }
-
-            for (let c of chars) {
-                if( !isNaN(Number(c)) || separators.includes(c)){
-                    valid = false
-                    break;
-                }
-            }
-
-            word = chars.join("");
+        while(chars.length > 0 && (separators.includes(chars[chars.length-1]) || !isNaN(Number(chars[chars.length-1])))){
+            chars.pop();
         }
-        else{
+        while(chars.length > 0 && (!isNaN(Number(chars[0])) || separators.includes(chars[0]))){
+            chars.shift();
+        }
+
+        for (let c of chars) {
+            if(!isNaN(Number(c)) || separators.includes(c)){
+                valid = false;
+                break;
+            }
+        }
+        word = chars.join("");
+        if(word.length < 2){
             valid = false;
         }
         if (valid){
@@ -30,11 +58,12 @@ export function validateWords(words){
     validWords.sort((a,b) => {
         return b.length - a.length;
     });
+    console.log(validWords);
     return validWords;
 }
 
-const separators = [",", ".", "/", "#", "№", "-", "+", "=", "(", ")", "*", "!", "@", "\"", "'",
-    "$",";", "^", ":", "&", "?", "[", "]", "{", "}", "\\", "_"
+const separators = [",", ".", "/", "#", "№", "-", "+", "=", "(", ")", "*", "!", "@", '"', "'",
+    "$",";", "^", ":", "&", "?", "[", "]", "{", "}", "\\", "_", "\n"
 ]
 
 function displayWords(words){
