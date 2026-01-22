a9aa00cb54b25d9045b427a383aa9a14d2105862
Sanek22228 <alexandrai20162008@gmail.com>
2026-01-22 13:43:31 +0300

    added styles for header, footer, publication page. information about crossword displays on the publication page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,12 +5,12 @@ import { NavLink, Outlet } from 'react-router-dom'
 function Layout(){
   return(
     <>
-      <header style={{background: "grey", textAlign: "center", padding: "1%", top: "0", position: "sticky", height: "25px"}}>
+      <header style={{background: "#939393", textAlign: "center", padding: ".7%", top: "0", position: "sticky", maxHeight: "120px", marginBottom: "20px", zIndex: 10}}>
         <NavLink to='/' className={({isActive}) => `navlink ${isActive ? 'active': ''}`}>Создать кроссворд</NavLink>
         <NavLink to='/publication' className={({isActive}) => `navlink ${isActive ? 'active' : ''}`}>Опубликовать</NavLink>
       </header>
       <Outlet></Outlet>
-      <footer>
+      <footer style={{width: "100%", background: "#AFAFAF", marginTop: "120px", bottom: 0, position: "relative"}}>
         Кроссворды
       </footer>
     </>
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import '../styles/App.css';
 import {validateWords} from '../utils/wordValidation'
 import { CreateCrossword } from '../utils/crosswordGenerator'
-import { CrosswordTable } from '../utils/crosswordVisualizer'
+import { CrosswordViewer } from '../utils/crosswordViewer'
 import { wordsErrorHandler } from '../utils/errorHandler';
 import infoIcon from '../images/info.png';
 import { useCrossword } from '../hook/useCrossword';
@@ -61,24 +61,28 @@ function Main(){
         <div>
           <h2>Введите слова:</h2>
           <br></br>
-          <textarea value={inputValue} autoFocus onChange={(e) => setInputValue(e.target.value)} type='text' name='wordInput' className='wordInput'></textarea>
-          <button style={{position: "relative"}} className='infoBtn' onMouseEnter={() => setHint(true)} onMouseLeave={() => setHint(false)}>
-              <img src={infoIcon} alt='info'></img>
-          </button>
-          {showHint && <span className='hint'>
-            Правила ввода слов:
-            <ul style={{margin: '5px 0', paddingLeft: '20px'}}>
-              <li>Только буквы русского/английского алфавита</li>
-              <li>Цифры и спецсимволы запрещены</li>
-              <li>Минимум 2 слова для создания кроссворда</li>
-              <li>Разделяйте слова пробелами или знаками препинания</li>  
-            </ul>
-          </span>}
+          <div style={{display: "flex", alignItems: "flex-start", flexDirection:"row", width: "100%"}}>
+            <textarea value={inputValue} autoFocus onChange={(e) => setInputValue(e.target.value)} type='text' name='wordInput' className='wordInput'></textarea>
+            <button className='infoBtn' onMouseEnter={() => setHint(true)} onMouseLeave={() => setHint(false)}>
+                <img src={infoIcon} alt='info'></img>
+            </button>
+            <div>
+            {showHint && <span className='hint'>
+              Правила ввода слов:
+              <ul style={{margin: '5px 0', paddingLeft: '20px'}}>
+                <li>Только буквы русского/английского алфавита</li>
+                <li>Цифры и спецсимволы запрещены</li>
+                <li>Минимум 2 слова для создания кроссворда</li>
+                <li>Разделяйте слова пробелами или знаками препинания</li>  
+              </ul>
+            </span>}
+            </div>
+          </div>
         </div>
         <button className='createBtn' onClick={HandleClick}>Создать</button>      
         {error && <p id='error'>{error}</p>}
         <div className='crosswordContainer'>
-          {crossword && <CrosswordTable crossword={crossword}/>}
+          {crossword && <CrosswordViewer crossword={crossword}/>}
         </div>
       </main>
     </>
