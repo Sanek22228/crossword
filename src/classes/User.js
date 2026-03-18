0e63f60285d74c8154f26d237c45b41453ad3a47
Sanek22228 <alexandrai20162008@gmail.com>
2026-03-18 12:23:15 +0300

    clear trash

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,10 +1,4 @@
 export class User{
-    // constructor(email, username, id, password){
-    //     this.Email = email;
-    //     this.UserName = username;
-    //     this.UserId = id;
-    //     this.Password = password;
-    // }
     constructor(email, password){
         this.Email = email;
         this.Password = password;
